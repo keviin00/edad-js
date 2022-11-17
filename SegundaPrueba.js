ea21bb1302323e3171ff39c90ad9d6573ad7d828
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-11-17 12:37:58 -0300

    Add files via upload

---
+++ SegundaPrueba.js
@@ -0,0 +1,14 @@
+let edad = parseInt(prompt("ingresa tu edad"));
+
+if (edad < 18) {
+    alert("Sos menor de 18");
+}
+else if ((edad > 18) && (edad < 60 )) {
+    alert("Ya podes laburar");
+}
+else if ((edad > 60) && (edad < 99 )){
+    alert("jubilate");
+}
+else if(edad =>100){
+    alert("No entiendo como vivis");
+}
