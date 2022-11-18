0285bf4a786fba042d11c92b06793df50f042a69
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-11-17 21:37:17 -0300

    Update SegundaPrueba.js

--- SegundaPrueba.js
+++ SegundaPrueba.js
@@ -3,12 +3,12 @@ let edad = parseInt(prompt("ingresa tu edad"));
 if (edad < 18) {
     alert("Sos menor de 18");
 }
-else if ((edad => 18) && (edad < 60 )) {
+else if ((edad >= 18) && (edad < 60 )) {
     alert("Ya podes laburar");
 }
-else if ((edad => 60) && (edad <= 99 )){
+else if ((edad >= 60) && (edad <= 99 )){
     alert("jubilate");
 }
-else if(edad =>100){
+else if(edad >=100){
     alert("No entiendo como vivis");
 }
